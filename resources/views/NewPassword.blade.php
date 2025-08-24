7a06c1208cd68cc1c6c581ff178094d0d0cac588
Akbargunawan <123akbargunawan@gmail.com>
2025-08-24 12:27:21 +0700

    config

--- resources/views/NewPassword.blade.php
+++ resources/views/NewPassword.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Buat Password Baru - MengABSEN</title>
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="min-h-screen bg-[#E87F2D] flex items-center justify-center relative overflow-hidden">
 
